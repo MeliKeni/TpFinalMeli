7d9e22ee93c0b72a58a59b98806274010cf6972f
MeliKeni <melinakenigsberg@gmail.com>
2025-11-21 18:43:40 -0300

    Initial commit

---
+++ Assets/Scripts/DetectorPiso.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectorPiso : MonoBehaviour
+{
+    public LuzRaycast RaycastScript;   // arrastrás el objeto ZonaSalto en el inspector
+    public bool enElPiso = false;
+        private Rigidbody rb;
+            public float force = 5f;
+
+
+    void Start(){
+                rb = GetComponent<Rigidbody>();
+    }
+    void Update(){
+        if(RaycastScript.zonaActivada==true){
+            if(enElPiso==true){
+            rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+            enElPiso=false;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Piso"))
+        {
+            enElPiso = true;
+        }
+    }
+}
---
+++ Assets/Scripts/LuzRaycast.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuzRaycast : MonoBehaviour
+{
+    public float distancia = 20f;
+    public float fuerzaSalto = 5f;
+    public bool zonaActivada = false;
+    private bool raycastActivo = false;
+    private float timer = 0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(raycastActivo){
+        timer -= Time.deltaTime;
+
+        Debug.DrawRay(transform.position, transform.forward * distancia, Color.red);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, distancia))
+        {
+            zonaActivada=true;
+        }
+       
+
+        if (timer <= 0f)
+            {
+                raycastActivo = false;
+                zonaActivada = false;
+            }
+        }
+    }
+      public void ActivarRayo()
+    {
+        raycastActivo = true;
+        zonaActivada = true;
+        timer = 5f;
+    }
+}
