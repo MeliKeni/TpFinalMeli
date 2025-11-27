a588228f052ec3b81ca0c1fbf722bee9bb1efee3
MeliKeni <melinakenigsberg@gmail.com>
2025-11-26 23:34:40 -0300

    pequenios avances

---
+++ Assets/Scripts/SeleccionarLuces.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeleccionarLuces : MonoBehaviour
+{
+    public GameObject panel;
+    public bool panelprendido=false;
+    
+    void Start(){
+        panel.SetActive(false);
+    }
+public void Panel(){
+    if(panelprendido == true){
+        panel.SetActive(false);
+        panelprendido=false;
+    } else if(panelprendido == false){
+        panel.SetActive(true);
+        panelprendido=true;
+    }
+}   
+}
---
+++ Assets/Scripts/SistemaLuces.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;using UnityEngine;
+using UnityEngine.UI;
+
+public class SistemaLuces : MonoBehaviour
+{
+    public Toggle[] toggles;       
+    public GameObject[] luces;     
+
+void Start(){
+     for (int i = 0; i < toggles.Length; i++)
+        {
+            toggles[i].isOn = false;
+        }
+}
+    public void AplicarLuces()
+    {
+        for (int i = 0; i < toggles.Length; i++)
+        {
+            bool encendida = toggles[i].isOn;
+            //luces[i].SetActive(encendida);
+        }
+
+          for (int i = 0; i < toggles.Length; i++)
+        {
+            toggles[i].isOn = false;
+            //luces[i].SetActive(false)
+        }
+    }
+}
